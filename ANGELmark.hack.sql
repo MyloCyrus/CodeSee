1db9c40bc72e14810c39bc785de134cac565e9a1
Mylo Cyrus <irahiggins740@gmail.com>
2023-06-26 20:08:54 -0700

    Create ANGELmark.hack.sql

---
+++ ANGELmark.hack.sql
@@ -0,0 +1,32 @@
+Microsoft Windows [Version 6.1.7601]
+Copyright (c) 2009 Microsoft Corporation.  All rights reserved.
+
+C:\Users\mylo>.hack
+'.hack' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\mylo>"C:\Users\mylo\New folder\japan_North_korea_hacked_by_ruesaia.hack
+.mc.shader"
+
+C:\Users\mylo>"C:\Users\mylo\New folder\japan_North_korea_hacked_by_ruesaia.hack
+.mc.shader"
+
+C:\Users\mylo>"C:\Users\mylo\New folder\facebook-RealMyloCyrus.py"
+
+C:\Users\mylo>
+"C:\Users\mylo\New folder\device_alt-break.js"
+
+C:\Users\mylo>[4584:0626/194041.070:ERROR:gl_surface_egl.cc(808)] EGL Driver mes
+sage (Error) eglCreateContext: Requested GLES version (3.0) is greater than max
+supported (2, 0).
+[4584:0626/194041.105:ERROR:gl_context_egl.cc(352)] eglCreateContext failed with
+ error EGL_SUCCESS
+[main 2023-06-27T02:41:04.704Z] update#ctor - updates are disabled due to runnin
+g as Admin in user setup
+[main 2023-06-27T02:41:21.476Z] CodeWindow: detected unresponsive
+[main 2023-06-27T02:42:35.829Z] Starting extension host with pid 5620 (fork() to
+ok 12 ms).
+[1280:0626/194238.846:ERROR:gpu_init.cc(446)] Passthrough is not supported, GL i
+s disabled, ANGLE is
+[main 2023-06-27T02:43:23.928Z] Enabling inspect port on extension host with pid
+ 5620.
