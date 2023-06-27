864dff994a5e77e27da87a021b5486c1128b5e63
Mylo Cyrus <irahiggins740@gmail.com>
2023-06-26 19:53:49 -0700

    Create network_file.js

---
+++ network_file.js
@@ -0,0 +1,35 @@
+Request URL: https://abs.twimg.com/responsive-web/client-web/i18n/en.6c8f3c4a.js
+Request Method: GET
+Status Code: 200  (from memory cache)
+Remote Address: [2606:2800:21f:5b71:3e29:d001:be46:4bcc]:443
+Referrer Policy: strict-origin-when-cross-origin
+accept-ranges: bytes
+access-control-allow-origin: *
+age: 291310
+content-encoding: gzip
+content-length: 118361
+content-type: application/javascript; charset=utf-8
+date: Mon, 26 Jun 2023 22:31:11 GMT
+etag: "UNEYqqYMrQQ1EQroKFc0sA=="
+expires: Tue, 25 Jun 2024 22:31:11 GMT
+last-modified: Fri, 23 Jun 2023 13:19:31 GMT
+perf: 7626143928
+server: ECAcc (sab/568C)
+strict-transport-security: max-age=631138519
+surrogate-key: twitter-assets
+timing-allow-origin: https://twitter.com, https://mobile.twitter.com
+vary: Accept-Encoding
+x-cache: HIT
+x-connection-hash: bfbf1d675b2ded080bd5b196bf71319771d57f6be9b949d7930666c24f60033c
+x-content-type-options: nosniff
+x-response-time: 28
+x-ton-expected-size: 390085
+x-transaction-id: 615b5a5a2f49e166
+Provisional headers are shown. Disable cache to see full headers.
+Learn more
+Origin: https://twitter.com
+Referer: https://twitter.com/
+sec-ch-ua: "Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"
+sec-ch-ua-mobile: ?0
+sec-ch-ua-platform: "Windows"
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36
